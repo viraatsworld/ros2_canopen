c0f733d2d8dd485cb7a7250c43dee8068e7675bd
Viraat <viraat.saaran@ipa.fraunhofer.de>
2025-08-02 13:00:33 +0200

    Combine canopen and cia402 system launch

--- canopen_tests/launch/canopen_system.launch.py
+++ canopen_tests/launch/canopen_system.launch.py
@@ -30,16 +30,11 @@
 # Author: Lovro Ivanov lovro.ivanov@gmail.com
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import OpaqueFunction
-from launch.actions import TimerAction
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, TimerAction, IncludeLaunchDescription
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution, PythonExpression
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
@@ -48,22 +43,26 @@ def launch_setup(context, *args, **kwargs):
     name = LaunchConfiguration("name")
     prefix = LaunchConfiguration("prefix")
 
+    #launch mode canopen_system or cia402_system
+    mode = LaunchConfiguration("mode")
+
+
     # bus configuration
     bus_config_package = LaunchConfiguration("bus_config_package")
-    bus_config_directory = LaunchConfiguration("bus_config_directory")
+    #dynamically set based on mode
+    config_directory =  PathJoinSubstitution(["config", mode])
     bus_config_file = LaunchConfiguration("bus_config_file")
     # bus configuration file full path
     bus_config = PathJoinSubstitution(
-        [FindPackageShare(bus_config_package), bus_config_directory, bus_config_file]
+        [FindPackageShare(bus_config_package), config_directory, bus_config_file]
     )
 
     # master configuration
     master_config_package = LaunchConfiguration("master_config_package")
-    master_config_directory = LaunchConfiguration("master_config_directory")
     master_config_file = LaunchConfiguration("master_config_file")
     # master configuration file full path
     master_config = PathJoinSubstitution(
-        [FindPackageShare(master_config_package), master_config_directory, master_config_file]
+        [FindPackageShare(master_config_package), config_directory, master_config_file]
     )
 
     # can interface name
@@ -71,13 +70,17 @@ def launch_setup(context, *args, **kwargs):
 
     # robot description stuff
     description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
+
+    # Setting urdf based on mode
+    string_mode = LaunchConfiguration("mode").perform(context)
+    description_file =  f"{string_mode}.urdf.xacro"
+
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", "canopen_system", description_file]
+                [FindPackageShare(description_package), "urdf", mode, description_file]
             ),
             " ",
             "name:=",
@@ -101,13 +104,12 @@ def launch_setup(context, *args, **kwargs):
 
     # ros2 control configuration
     ros2_control_config_package = LaunchConfiguration("ros2_control_config_package")
-    ros2_control_config_directory = LaunchConfiguration("ros2_control_config_directory")
     ros2_control_config_file = LaunchConfiguration("ros2_control_config_file")
     # ros2 control configuration file full path
     ros2_control_config = PathJoinSubstitution(
         [
             FindPackageShare(ros2_control_config_package),
-            ros2_control_config_directory,
+            config_directory,
             ros2_control_config_file,
         ]
     )
@@ -126,7 +128,6 @@ def launch_setup(context, *args, **kwargs):
     actions=[control_node],
         )
 
-
     # load one controller just to make sure it can connect to controller_manager
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -138,6 +139,28 @@ def launch_setup(context, *args, **kwargs):
         package="controller_manager",
         executable="spawner",
         arguments=["node_1_controller", "--controller-manager", "/controller_manager"],
+        condition=IfCondition(PythonExpression([
+            "'", mode, "' == 'canopen_system'"
+        ])),
+    )
+
+
+    cia402_device_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["cia402_device_1_controller", "--controller-manager", "/controller_manager"],
+        condition=IfCondition(PythonExpression([
+            "'", mode, "' == 'cia402_system'"
+        ])),
+    )
+
+    forward_position_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+        condition=IfCondition(PythonExpression([
+            "'", mode, "' == 'cia402_system'"
+        ])),
     )
 
     robot_state_publisher_node = Node(
@@ -148,29 +171,56 @@ def launch_setup(context, *args, **kwargs):
     )
 
     # hardcoded slave configuration form test package
-    slave_config = PathJoinSubstitution(
-        [FindPackageShare(master_config_package), master_config_directory, "simple.eds"]
+    canopen_system_slave_config = PathJoinSubstitution(
+        [FindPackageShare(master_config_package), "config/canopen_system", "simple.eds"]
     )
 
-    slave_launch = PathJoinSubstitution(
+    cia402_system_slave_config = PathJoinSubstitution(
+        [FindPackageShare("canopen_tests"), "config/cia402", "cia402_slave.eds"]
+    )
+
+    canopen_system_slave_launch = PathJoinSubstitution(
         [FindPackageShare("canopen_fake_slaves"), "launch", "basic_slave.launch.py"]
     )
+
+    cia402_system_slave_launch = PathJoinSubstitution(
+        [FindPackageShare("canopen_fake_slaves"), "launch", "cia402_slave.launch.py"]
+    )
     slave_node_1 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(slave_launch),
+        PythonLaunchDescriptionSource(canopen_system_slave_launch),
         launch_arguments={
             "node_id": "2",
             "node_name": "slave_node_2",
-            "slave_config": slave_config,
+            "slave_config": canopen_system_slave_config,
         }.items(),
+        condition=IfCondition(PythonExpression([
+            "'", mode, "' == 'canopen_system'"
+        ])),
     )
 
     slave_node_2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(slave_launch),
+        PythonLaunchDescriptionSource(canopen_system_slave_launch),
         launch_arguments={
             "node_id": "3",
             "node_name": "slave_node_3",
-            "slave_config": slave_config,
+            "slave_config": canopen_system_slave_config,
         }.items(),
+        condition=IfCondition(PythonExpression([
+            "'", mode, "' == 'canopen_system'"
+        ])),
+    )
+
+    #cia402system
+    slave_node_3 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(cia402_system_slave_launch),
+        launch_arguments={
+            "node_id": "2",
+            "node_name": "cia402_node_1",
+            "slave_config": cia402_system_slave_config,
+        }.items(),
+        condition=IfCondition(PythonExpression([
+            "'", mode, "' == 'cia402_system'"
+        ])),
     )
 
     nodes_to_start = [
@@ -178,8 +228,11 @@ def launch_setup(context, *args, **kwargs):
         joint_state_broadcaster_spawner,
         slave_node_1,
         slave_node_2,
+        slave_node_3,
         delayed_control_node,
+        forward_position_controller,
         canopen_proxy_controller_spawner,
+        cia402_device_controller_spawner,
     ]
 
     return nodes_to_start
@@ -203,11 +256,29 @@ def generate_launch_description():
             default_value="canopen_tests",
         )
     )
+
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_file",
+            name="mode",
+            default_value=TextSubstitution(text="canopen_system"),
+            choices=["canopen_system", "cia402_system"],
+            description="Select system mode"
+        ),
+
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "canopen_system_description_file",
             description="Name of the urdf file.",
             default_value="canopen_system.urdf.xacro",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "cia402_system_description_file",
+            description="Name of the urdf file.",
+            default_value="cia402_system.urdf.xacro",
         )
     )
     declared_arguments.append(
@@ -217,13 +288,7 @@ def generate_launch_description():
             description="Path to ros2_control configuration.",
         )
     )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "ros2_control_config_directory",
-            default_value="config/canopen_system",
-            description="Path to ros2_control configuration.",
-        )
-    )
+
     declared_arguments.append(
         DeclareLaunchArgument(
             "ros2_control_config_file",
@@ -240,13 +305,6 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "bus_config_directory",
-            default_value="config/canopen_system",
-            description="Path to bus configuration.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "bus_config_file",
             default_value="bus.yml",
             description="Path to bus configuration.",
@@ -256,13 +314,6 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "master_config_package",
             default_value="canopen_tests",
-            description="Path to master configuration file (*.dcf)",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "master_config_directory",
-            default_value="config/canopen_system",
             description="Path to master configuration file (*.dcf)",
         )
     )
